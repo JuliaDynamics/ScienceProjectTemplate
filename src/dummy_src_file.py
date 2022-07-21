2b72fe43f2af925ba5785f95ad5b3ac7600a879c
diegojco <diego.jimenez@mpimet.mpg.de>
2022-07-21 10:24:44 +0200

    Added a dummy function for python

---
+++ src/dummy_src_file.py
@@ -0,0 +1,10 @@
+"""
+    dummy_project_function(x, y) → z
+Dummy function for illustration purposes.
+Performs operation:
+```math
+z = x + y
+```
+"""
+def dummy_project_function(x, y):
+    return x + y
